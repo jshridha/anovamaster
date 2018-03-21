5e0257c6be5915f515d88230c91e06dfa4423d2f
Peter Hardy <peter@hardy.dropbear.id.au>
2018-03-21 21:04:50 +1100

    Fetching and dumping current status.

--- AnovaMaster.py
+++ AnovaMaster.py
@@ -29,7 +29,7 @@ class AnovaConfiguration(ConfigParser):
         if (not self.has_option('anova', 'verbose')):
             self.set('anova', 'verbose', False)
 
-class AnovaStatus:
+class AnovaStatus(object):
     def __init__(self):
         self.temp_unit = "C"
         self.current_temp = "0"
@@ -48,9 +48,8 @@ class AnovaMaster:
         self.connect()
 
     def connect(self):
-        if (self._config.get('anova', 'verbose')):
-            print("Trying to connect to {}".format(self._config.get('anova', 'mac')))
         if (self._status.state is "disconnected"):
+            self.debug_log("Trying to connect to {}".format(self._config.get('anova', 'mac')))
             try:
                 self._anova.connect()
                 self._status.state = "stopped"
@@ -59,10 +58,12 @@ class AnovaMaster:
                 # Assuming this was because the device is off / out of range
                 print("Can't connect to Anova. Is it on and in range?")
                 self._status.state = "disconnected"
+        else:
+            self.debug_log("Not reconnecting, as we're already connected")
 
     def fetch_status(self):
         # Update our internal state with the current status from the anova
-        self.connect()
+        #self.connect()
         if (self._status.state is not "disconnected"):
             try:
                 anova_status = self._anova.anova_status()
@@ -83,8 +84,18 @@ class AnovaMaster:
                 self._status.current_temp = self._anova.read_temp()
             except bluepy.btle.BTLEException:
                 print("Failed to receive state. Assuming we're disconnected.")
-                self._status.state = AnovaStatus.disconnected
+                self._status.state = 'disconnected'
+
+    def dump_status(self):
+        json_status = json.dumps(self._status.__dict__, sort_keys=True,
+                                 indent=4)
+        print(json_status)
+
+    def debug_log(self, str):
+        if (self._config.get('anova', 'verbose')):
+            print(str)
 
 if __name__ == '__main__':
     config = AnovaConfiguration()
     my_anova = AnovaMaster(config)
+    my_anova.dump_status()
